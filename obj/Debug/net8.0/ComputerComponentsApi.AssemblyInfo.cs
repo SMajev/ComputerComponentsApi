7ccd21bb25603e1f139f626fa0dae4a8a0b4b023
mekh <smajev@protonmail.com>
2026-05-23 16:03:21 +0200

    Finish
    Signed-off-by: mekh <smajev@protonmail.com>

--- obj/Debug/net8.0/ComputerComponentsApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/ComputerComponentsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComputerComponentsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7faa5a756fd4a5e7dc8123fd8dbffa9995c87003")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComputerComponentsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComputerComponentsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
