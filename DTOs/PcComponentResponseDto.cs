af8b694636597b2cf6d7d5380e7fae03ffde23ed
mekh <smajev@protonmail.com>
2026-05-23 15:43:13 +0200

    Dtos
    Signed-off-by: mekh <smajev@protonmail.com>

---
+++ DTOs/PcComponentResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ComputerComponentsApi.DTOs;
+
+public class PcComponentResponseDto
+{
+    public string ComponentCode { get; set; } = null!;
+    public string ComponentName { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public string Manufacturer { get; set; } = null!;
+    public string Type { get; set; } = null!;
+    public int Amount { get; set; }
+}
---
+++ DTOs/PcRequestDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComputerComponentsApi.DTOs;
+
+public class PcRequestDto
+{
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; } = null!;
+
+    [Range(0.1, 9999)]
+    public decimal Weight { get; set; }
+
+    [Range(0, 120)]
+    public int Warranty { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    [Range(0, int.MaxValue)]
+    public int Stock { get; set; }
+}
