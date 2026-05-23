ac2fb8f9a3b24e2939138f0eee6ce799b59ee20d
mekh <smajev@protonmail.com>
2026-05-23 15:41:31 +0200

    Models
    Signed-off-by: mekh <smajev@protonmail.com>

---
+++ models/PcComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace ComputerComponentsApi.Models;
+
+public class PcComponent
+{
+    public int PcId { get; set; }
+    public string ComponentCode { get; set; } = null!;
+    public int Amount { get; set; }
+
+    public Pc Pc { get; set; } = null!;
+    public Component Component { get; set; } = null!;
+}
